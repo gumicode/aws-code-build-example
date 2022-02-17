22672a5437385e13cb2ddb222b239c5d97809027
gumicode <gumicode@gmail.com>
2022-02-17 15:17:24 +0900

    add project

---
+++ src/main/java/com/example/awscodebuildexample/AwsCodeBuildExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscodebuildexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodeBuildExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCodeBuildExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awscodebuildexample/AwsCodeBuildExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscodebuildexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodeBuildExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
